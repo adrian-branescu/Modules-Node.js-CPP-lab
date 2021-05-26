21b79f6854092347f5b7dcb784fe236eef6e4e90
Adrian Branescu <adrianbr@intern.pch>
2021-05-26 11:33:23 +0300

    Illustrate cross-platform ES Module

--- lab/modules/www/static/helperModule.js
+++ lab/modules/www/static/helperModule.js
@@ -1,3 +1,7 @@
+// Unfortunately, browser implementation of ES Module loader doesn't include a module resolver like in Node.js
+// So, import { encodeBase64 } from 'esm-example' no longer work. You have to specify full / relative URL.
+import { encodeBase64 } from '../node_modules/esm-example/index.js';
+
 function setElementSrc(resourceUrl, resourceLabel, elementId) {
 
     const responsePromise = fetch(resourceUrl);
@@ -21,9 +25,7 @@ function setElementSrc(resourceUrl, resourceLabel, elementId) {
         const contentType = response.headers.get('content-type');
 
         const arrayBuffer = await response.arrayBuffer();
-        const bytesArray = new Uint8Array(arrayBuffer);
-        const bytesString = bytesArray.reduce((acc, byte) => acc + String.fromCharCode(byte), '');
-        const encodedData = btoa(bytesString);
+        const encodedData = encodeBase64(btoa, arrayBuffer);
 
         const element = document.getElementById(elementId);
         element.src = `${protocol}:${contentType};${encoding},${encodedData}`;
